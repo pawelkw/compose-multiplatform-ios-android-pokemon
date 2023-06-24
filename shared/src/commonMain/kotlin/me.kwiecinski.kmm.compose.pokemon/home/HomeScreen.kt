1b91e22c52ac5f49f227b9b0dd3b748acfdff364
Paweł Kwieciński <kijupl@gmail.com>
2023-06-24 09:09:38 +0200

    Allow back navigation via top bar back button.

--- shared/src/commonMain/kotlin/me.kwiecinski.kmm.compose.pokemon/home/HomeScreen.kt
+++ shared/src/commonMain/kotlin/me.kwiecinski.kmm.compose.pokemon/home/HomeScreen.kt
@@ -3,6 +3,7 @@ package me.kwiecinski.kmm.compose.pokemon.home
 import androidx.compose.runtime.Composable
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.slide
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.stackAnimation
+import com.arkivanov.decompose.router.stack.pop
 import com.arkivanov.decompose.router.stack.push
 import io.github.xxfast.decompose.router.Router
 import io.github.xxfast.decompose.router.content.RoutedContent
@@ -22,7 +23,7 @@ fun HomeScreen() {
   ) { screen ->
     when (screen) {
       List -> PokemonList(onPokemonClicked = { pokemonId -> router.push(Details(pokemonId)) })
-      is Details -> PokemonDetail(pokemonId = screen.pokemonId)
+      is Details -> PokemonDetail(pokemonId = screen.pokemonId) { router.pop() }
     }
   }
 }
--- shared/src/commonMain/kotlin/me.kwiecinski.kmm.compose.pokemon/pokemondetail/PokemonDetail.kt
+++ shared/src/commonMain/kotlin/me.kwiecinski.kmm.compose.pokemon/pokemondetail/PokemonDetail.kt
@@ -1,25 +1,56 @@
 package me.kwiecinski.kmm.compose.pokemon.pokemondetail
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import me.kwiecinski.kmm.compose.pokemon.pokemonlist.PokemonViewModel
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import org.koin.compose.koinInject
 
 @Composable
 fun PokemonDetail(
     modifier: Modifier = Modifier,
     viewModel: PokemonDetailViewModel = koinInject(),
-    pokemonId: Int
+    pokemonId: Int,
+    onBackClicked: () -> Unit,
 ) {
     val state by viewModel.state.collectAsState()
 
     remember(pokemonId) {
         viewModel.handleIntent(PokemonDetailIntent.Initial(pokemonId))
     }
-
-    Text(state.name)
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Pokemon Details", textAlign = TextAlign.Center)
+                },
+                navigationIcon = {
+                    IconButton(onClick = onBackClicked) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                }
+            )
+        }
+    ) {
+        Box(modifier = Modifier.padding(16.dp)) {
+            Text(text = state.name, style = MaterialTheme.typography.h4)
+        }
+    }
 }
