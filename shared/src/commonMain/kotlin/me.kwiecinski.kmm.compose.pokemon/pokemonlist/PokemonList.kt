7647b6930bff310948d8411972a65ca45200896d
Paweł Kwieciński <kijupl@gmail.com>
2023-06-25 18:59:44 +0200

    Pull to refresh

--- shared/src/commonMain/kotlin/me.kwiecinski.kmm.compose.pokemon/pokemonlist/PokemonList.kt
+++ shared/src/commonMain/kotlin/me.kwiecinski.kmm.compose.pokemon/pokemonlist/PokemonList.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -18,11 +19,21 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.unit.dp
+import app.cash.paging.LoadState
+import app.cash.paging.LoadStateLoading
+import app.cash.paging.LoadStateNotLoading
 import app.cash.paging.compose.collectAsLazyPagingItems
 import app.cash.paging.compose.itemKey
 import io.kamel.image.KamelImage
@@ -30,6 +41,7 @@ import io.kamel.image.asyncPainterResource
 import me.kwiecinski.kmm.compose.pokemon.pokemondetail.PokemonDetail
 import org.koin.compose.koinInject
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun PokemonList(
     modifier: Modifier = Modifier,
@@ -38,52 +50,66 @@ fun PokemonList(
 ) {
     val lazyColumnState = rememberLazyListState()
     val lazyPagingItems = viewModel.pagerFlow.collectAsLazyPagingItems()
+    var refreshing by remember { mutableStateOf(false) }
+
+    val pullRefreshState = rememberPullRefreshState(refreshing, {
+        lazyPagingItems.refresh()
+    })
 
     Column(modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-        LazyColumn(modifier = Modifier.fillMaxHeight(), state = lazyColumnState) {
-            items(
-                count = lazyPagingItems.itemCount,
-                key = lazyPagingItems.itemKey { it.pokemon.url }
-            ) { index ->
-                val item = lazyPagingItems[index]
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable {
-                            onPokemonClicked(item!!.pokemon.pokemonId())
-                        }
-                        .padding(horizontal = 16.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    KamelImage(
-                        modifier = Modifier.size(80.dp),
-                        resource = asyncPainterResource(
-                            data = item!!.pokemon.spriteUrl(),
-                            key = item.pokemon.url
-                        ),
-                        contentDescription = "pokemon image"
-                    )
-                    Text(
+        Box(Modifier.pullRefresh(pullRefreshState)) {
+            LazyColumn(modifier = Modifier.fillMaxHeight(), state = lazyColumnState) {
+                when (lazyPagingItems.loadState.refresh) {
+                    is LoadStateLoading -> { refreshing = true }
+                    else -> { refreshing = false }
+                }
+                items(
+                    count = lazyPagingItems.itemCount,
+                    key = lazyPagingItems.itemKey { it.pokemon.url }
+                ) { index ->
+                    val item = lazyPagingItems[index]
+                    Row(
                         modifier = Modifier
-                            .padding(start = 20.dp),
-                        text = item.pokemon.name.capitalize(),
-                        style = MaterialTheme.typography.h5,
-                    )
-                    Box(modifier = Modifier.fillMaxWidth()) {
-                        IconButton(
-                            modifier = Modifier.align(alignment = Alignment.CenterEnd),
-                            onClick = { viewModel.onAddFavouriteClicked(item.pokemon.pokemonId())
-                                lazyPagingItems.refresh()
-                            }) {
-                            Icon(
-                                imageVector = if(item.isFavourite) Icons.Outlined.Favorite else Icons.Outlined.FavoriteBorder,
-                                contentDescription = "add/remove to/from favourites"
-                            )
+                            .fillMaxWidth()
+                            .clickable {
+                                onPokemonClicked(item!!.pokemon.pokemonId())
+                            }
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        KamelImage(
+                            modifier = Modifier.size(80.dp),
+                            resource = asyncPainterResource(
+                                data = item!!.pokemon.spriteUrl(),
+                                key = item.pokemon.url
+                            ),
+                            contentDescription = "pokemon image"
+                        )
+                        Text(
+                            modifier = Modifier
+                                .padding(start = 20.dp),
+                            text = item.pokemon.name.capitalize(),
+                            style = MaterialTheme.typography.h5,
+                        )
+                        Box(modifier = Modifier.fillMaxWidth()) {
+                            IconButton(
+                                modifier = Modifier.align(alignment = Alignment.CenterEnd),
+                                onClick = {
+                                    viewModel.onAddFavouriteClicked(item.pokemon.pokemonId())
+                                    lazyPagingItems.refresh()
+                                }) {
+                                Icon(
+                                    imageVector = if (item.isFavourite) Icons.Outlined.Favorite else Icons.Outlined.FavoriteBorder,
+                                    contentDescription = "add/remove to/from favourites"
+                                )
+                            }
                         }
                     }
-                }
 
+                }
             }
+            PullRefreshIndicator(refreshing, pullRefreshState, Modifier.align(Alignment.TopCenter))
+
         }
     }
 }
